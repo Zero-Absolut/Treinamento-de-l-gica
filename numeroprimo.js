6a9fd3f6068e01ddbbcb2a74d530cdc85e9f2a5c
Mateus Brito <lestarfelipe@gmail.com>
2025-10-19 21:31:38 -0300

    Add files via upload

---
+++ numeroprimo.js
@@ -0,0 +1,23 @@
+
+function verificarPrimo(n) {
+ 
+    if (n <= 1) {
+        return false; 
+    }
+
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        
+        
+        if (n % i === 0) {
+            return false;
+        }
+    }
+
+   
+    return true;
+}
+
+
+
+
+console.log(verificarPrimo(13)); 
